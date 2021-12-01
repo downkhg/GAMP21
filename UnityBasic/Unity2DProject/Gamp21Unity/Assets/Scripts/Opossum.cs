4b92295a1ea5a312c10e4371dd282bad7129a75e
downkhg <downkhg@gmail.com>
2021-12-01 22:59:53 +0900

    몬스터 추가

---
+++ UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Opossum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opossum : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+            Destroy(collision.gameObject);
+    }
+}
