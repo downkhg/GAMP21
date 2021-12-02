b97fa6fd2993f5e167c769168bccb9af70d06c26
downkhg <downkhg@gmail.com>
2021-12-02 21:50:12 +0900

    총알발사

---
+++ UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject prefabBullet;
+    public GameObject objBullet;
+    public float ShotPower;
+
+    public void Shot()
+    {
+        GameObject copyBullet = Instantiate(prefabBullet); 
+        Rigidbody2D rigidbody = copyBullet.GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(Vector3.right * ShotPower);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Shot();
+    }
+}
