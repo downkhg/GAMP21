ee35ed8b4bdeea1c171a0c5bc2c39a1cf652cdcf
downkhg <downkhg@gmail.com>
2021-12-13 21:50:03 +0900

    물리없는 충돌체크

--- UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Eagle.cs
+++ UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Eagle.cs
@@ -6,17 +6,29 @@ public class Eagle : MonoBehaviour
 {
     public GameObject objTarget;
     public float Speed;
+    public float Site = 0.5f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireSphere(this.transform.position, Site);
+    }
+
+    private void FixedUpdate()
+    {
+        int nLayer = 1 << LayerMask.NameToLayer("Player");
+        Collider2D collider = 
+            Physics2D.OverlapCircle(this.transform.position, Site, nLayer);
+
+        if(collider)
+        {
+            objTarget = collider.gameObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (objTarget)
+        if (objTarget != null)
         {
             Vector3 vTargetPos = objTarget.transform.position;
             Vector3 vPos = this.transform.position;
