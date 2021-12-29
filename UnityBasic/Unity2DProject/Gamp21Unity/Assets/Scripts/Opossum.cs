e39bee6baaf51054713245a53fdd0484ca1e4dcb
downkhg <downkhg@gmail.com>
2021-12-29 20:01:00 +0900

    물리연산 선택적으로 적용하기

--- UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Opossum.cs
+++ UnityBasic/Unity2DProject/Gamp21Unity/Assets/Scripts/Opossum.cs
@@ -16,6 +16,26 @@ public class Opossum : MonoBehaviour
         transform.position += Vector3.left * Time.deltaTime;
     }
 
+    private void FixedUpdate()
+    {
+        int nLayer = 1 << LayerMask.NameToLayer("Player");
+        BoxCollider2D boxCollider = this.gameObject.GetComponent<BoxCollider2D>();
+        Collider2D collider = 
+            Physics2D.OverlapBox(this.transform.position, boxCollider.size, 0, nLayer);
+        if (collider)
+        {
+            Player target = collider.gameObject.GetComponent<Player>();
+            Player attaker = this.gameObject.GetComponent<Player>();
+            SuperMode superMode = target.GetComponent<SuperMode>();
+
+            if (superMode.isUse == false)
+            {
+                attaker.Attack(target);
+                superMode.OnMode();
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
