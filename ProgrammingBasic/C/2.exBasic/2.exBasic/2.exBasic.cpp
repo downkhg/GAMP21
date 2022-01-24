aad03938befb206cb714d790d93a0c80785b9676
downkhg <downkhg@gmail.com>
2022-01-24 21:02:49 +0900

    이중 for문을 이용한 그리기

--- ProgrammingBasic/C/2.exBasic/2.exBasic/2.exBasic.cpp
+++ ProgrammingBasic/C/2.exBasic/2.exBasic/2.exBasic.cpp
@@ -6,62 +6,61 @@
 //1.데이터와 알고리즘을 분리한다.
 //2.데이터는 변수 알고리즘은 연산,반복,조건 등이 조합된다.
 //3.반복되는 경우는 먼저 반복하고 정답을 출력한다.
-/*
-void BateriaMain()
-{
-	int nBateria = 10;
-	int nTime = 0;
 
-	nBateria = nBateria * 2;//20
-	nTime = 1;
+//void BateriaMain()
+//{
+//	int nBateria = 10;
+//	int nTime = 0;
+//
+//	nBateria = nBateria * 2;//20
+//	nTime = 1;
+//
+//	nBateria = nBateria * 2;//40
+//	nTime = 2;
+//
+//	nBateria = nBateria * 2;//80
+//	nTime = 3;
+//
+//	nBateria = nBateria * 2;//160
+//	nTime = 4;
+//
+//	printf("Time:%d\n", nTime);
+//}
 
-	nBateria = nBateria * 2;//40
-	nTime = 2;
-
-	nBateria = nBateria * 2;//80
-	nTime = 3;
-
-	nBateria = nBateria * 2;//160
-	nTime = 4;
-
-	printf("Time:%d\n", nTime);
-}
-*/
 //4.반복되는 규칙성을 찾아서 무한반복을 돌린후 반복된 과정을 모두 확인한다.
 //(프로그래머가 아니라면 여기서 마쳐도 무방함. 그러나 정치질(약점잡아 늘어지기)에 희생량이 될수있음)
-/*
 void BateriaMain()
 {
-	int nBateria = 10;
-	int nTime = 0;
+	int nBateria = 10; //10
+	int nTime = 0; //0
 
 	while (true)
 	{
-		nBateria = nBateria * 2;//20
-		nTime = nTime + 1;
-		printf("Bateria:%d Time:%d\n", nBateria, nTime);
-		if (nBateria > 100) break;
+		nBateria = nBateria * 2;//10 * 2 = 20 //20 * 2 = 40 //40 * 2 = 80 //80*2 = 160
+		nTime = nTime + 1; //1 //2 //3 //4
+		printf("Bateria:%d Time:%d\n", nBateria, nTime); //20 , 1 //40, 2
+		if (nBateria > 100) break; //20 > 100 -> F //40 > 100 -> F //80 > 100 -> F // 160 > 100 -> T
 	}
 }
-*/
+
 //5.break문의 조건을 반대로 if문에 넣는다.(초과,미만:불포함  이상,이하:포함)
 //세균은 10마리를 배양하고 세균수가 100마리 이상이 되는 시간은?
 //세균은 1시간마다 2배씩 증가한다.
 //데이터: 세균수,시간
 //알고리즘: 세균수*2, 1시간마다, 100마리 이상이될때까지
-void BateriaMain()
-{
-	int nBateria = 10;
-	int nTime = 0;
-
-	while (nBateria <= 100)
-	{
-		nBateria = nBateria * 2;//20
-		nTime = nTime + 1;
-		printf("Bateria:%d Time:%d\n", nBateria, nTime);
-		//if (nBateria > 100) break;
-	}
-}
+//void BateriaMain()
+//{
+//	int nBateria = 10;
+//	int nTime = 0;
+//
+//	while (nBateria <= 100)
+//	{
+//		nBateria = nBateria * 2;//20
+//		nTime = nTime + 1;
+//		printf("Bateria:%d Time:%d\n", nBateria, nTime);
+//		//if (nBateria > 100) break;
+//	}
+//}
 
 //종이를 접으면 (면적)이 (1/2로 줄어)들때,
 //(종이면적이 1/10이하)로 줄어들려면, (몇번접)아야하는가?
@@ -96,6 +95,8 @@ void PaperAreaMain()
 //(세균:변수) 10마리를 배양하여 (10시간후:조건)의 세균수를 구하여라.
 //이때, 세균이 (1시간 마다 4배)찍 증가한다 
 //세균수:10 //시간:0  //세균수*=4
+//데이터: 세균수, 시간  
+//알고리즘: 세균수*4 10시간후 시간 10보다 클때
 void BateriaCountMain()
 {
 
@@ -138,7 +139,8 @@ y01234x
 //2#####\n
 //3#####\n
 //4#####\n
-void RectMain()
+
+void FullMain()
 {
 	for (int y = 0; y < 5; y++)//0 ; 0<5->T ; 0++ // 1<5->T
 	{
@@ -149,9 +151,24 @@ void RectMain()
 		printf("\n");
 	}
 }
+
+void RectMain()
+{
+	for (int y = 0; y < 5; y++)//0 ; 0<5->T ; 0++ // 1<5->T
+	{
+		for (int x = 0; x < 5; x++)//0; 0<5;0++ // 1 < 5->T ; 1++// 2 < 5->T; 2++ // 3<5->T; 3++ //4<5->T 4++ //5<5->F
+		{
+			if ((y >= 1 && y <= 3) && (x >= 1 && x <= 3))
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
+}
 void WindowMain()
 {
-
+	
 }
 void StiarMain()
 {
@@ -161,5 +178,7 @@ void StiarMain()
 void main()
 {
 	//BateriaMain();
-	PaperAreaMain();
+	//PaperAreaMain();
+	//RectMain();
+	WindowMain();
 }
