a3a5dce070f1f494fe4b8bbc9c6eb19407179532
downkhg <downkhg@gmail.com>
2022-02-04 21:54:36 +0900

    포인터와 함수

---
+++ ProgrammingBasic/C/5.PointerArrayFuncntion/5.PointerArrayFuncntion/5.PointerArrayFuncntion.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+//데이터의 값이 전위 연산자와 후위연산자의 차이점을 확인하는 프로그램.
+void ValueAndOperatorMain();
+//정수와 실수의 데이터를 선언하고, 각 선언된 데이터를 각각의 실수와 포인터에 저장하여,
+//모든 변수들에서 연산할 수있는 모든 결과를 출력하여라.
+void PointerMain();
+
+void SwapVal(int a, int b);
+void SwapPtr(int* a, int* b);
+void SwapRef(int& a, int& b);
+
+void FunctionAndPointerMain();
+
+void main() 
+{
+	//ValueAndOperatorMain();
+	//PointerMain();
+	FunctionAndPointerMain();
+}
+
+void ValueAndOperatorMain()
+{
+	int nDataA = 10;
+	int nDataB = nDataA;
+	printf("1.DataA:%d\n", nDataA);
+	printf("1.DataB:%d\n", nDataB);
+	nDataB = nDataA++;
+	printf("2.DataA:%d\n", nDataA);
+	printf("2.DataB:%d\n", nDataB);
+	nDataB = --nDataA;
+	printf("3.DataA:%d\n", nDataA);
+	printf("3.DataB:%d\n", nDataB);
+}
+//정수와 실수의 데이터를 선언하고, 각 선언된 데이터를 각각의 실수와 포인터에 저장하여,
+//모든 변수들에서 연산할 수있는 모든 결과를 출력하여라.
+void PointerMain()
+{
+	int nData = 10;
+	float fData = 3.14f;
+	int* pIntData = &nData;//포인터: 주소값을 저장하는 변수
+	float* pFloatData = &fData;
+
+	printf("nData[%d]:%d\n", &nData, nData);
+	printf("fData[%d]:%f\n", &fData, fData);
+	//*(참조):포인터가 가진 주소값이 가르키는 변수의 값.
+	printf("IntData[%d]:%d\n", *pIntData, pIntData);
+	printf("FloatData[%f]:%d\n", *pFloatData, pFloatData);
+}
+
+void SwapVal(int a, int b)//값에의한 호출
+{
+	printf("SwapVal[%d]%d<->[%d]%d\n", &a, a, &b, b);
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void SwapPtr(int* a, int* b)//주소값에의한 호울
+{
+	printf("SwapPtr[%d]%d<->[%d]%d\n", *a, a, *b, b);
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void SwapRef(int& a, int& b)//참조에의한 호출
+{
+	printf("SwapRef[%d]%d<->[%d]%d\n", &a, a, &b, b);
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void FunctionAndPointerMain()
+{
+	int nDataA = 10;
+	int nDataB = 20;
+	printf("1.Origin[%d]%d<->[%d]%d\n", &nDataA, nDataA, &nDataB, nDataB);
+	SwapVal(nDataA, nDataB);
+	printf("2.SwapVal[%d]%d<->[%d]%d\n", &nDataA, nDataA, &nDataB, nDataB);
+	SwapPtr(&nDataA, &nDataB);
+	printf("3.SwapPtr[%d]%d<->[%d]%d\n", &nDataA, nDataA, &nDataB, nDataB);
+	SwapRef(nDataA, nDataB);
+	printf("4.SwapRef[%d]%d<->[%d]%d\n", &nDataA, nDataA, &nDataB, nDataB);
+}
