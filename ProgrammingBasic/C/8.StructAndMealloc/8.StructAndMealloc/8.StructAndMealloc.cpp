9bd57149184da15daecaf5fd8b6e389325495940
downkhg <downkhg@gmail.com>
2022-02-15 17:05:14 +0900

    참조자 포인터 분리

--- ProgrammingBasic/C/8.StructAndMealloc/8.StructAndMealloc/8.StructAndMealloc.cpp
+++ ProgrammingBasic/C/8.StructAndMealloc/8.StructAndMealloc/8.StructAndMealloc.cpp
@@ -9,36 +9,36 @@ struct SGameObject
 	int nLayer;
 };
 
-////매개변수는 포인터를 전달해야 원본을 변경가능하다.
-//void InitGameObject(SGameObject* obj, const char* name, const char* tag, int layer)
-//{
-//	strcpy_s(obj->strName, name);//->: 구조체의 포인터에서는 멤버를 접근할때 사용
-//	strcpy_s(obj->strTag, tag);
-//	obj->nLayer = layer;
-//}
-////원칙적으로는 매개변수로 데이터를 받아서 원본의 값을 변경해야한다.
-//void ToStringGameObject(char* strMsg, int size, const SGameObject* obj)
-//{
-//	sprintf_s(strMsg, size,"Name:%s, Tag:%s, Layer:%d", obj->strName, obj->strTag, obj->nLayer);
-//}
+//매개변수는 포인터를 전달해야 원본을 변경가능하다.
+void InitGameObjectPtr(SGameObject* obj, const char* name, const char* tag, int layer)
+{
+	strcpy_s(obj->strName, name);//->: 구조체의 포인터에서는 멤버를 접근할때 사용
+	strcpy_s(obj->strTag, tag);
+	obj->nLayer = layer;
+}
+//원칙적으로는 매개변수로 데이터를 받아서 원본의 값을 변경해야한다.
+void ToStringGameObjectPtr(char* strMsg, int size, const SGameObject* obj)
+{
+	sprintf_s(strMsg, size,"Name:%s, Tag:%s, Layer:%d", obj->strName, obj->strTag, obj->nLayer);
+}
 
 //리턴하여 다음과 같이 지역변수의 메모리를 참조하는 경우 문제가 발생가능성이 높다.
-//const char* ToStringGameObject(SGameObject obj)
-//{
-//	static char strMsg[2052];
-//	sprintf_s(strMsg, "Name:%s, Tag:%s, Layer:%d", obj.strName, obj.strTag, obj.nLayer);
-//	return strMsg;
-//}
+const char* ToStringGameObject(SGameObject obj)
+{
+	static char strMsg[2052];
+	sprintf_s(strMsg, "Name:%s, Tag:%s, Layer:%d", obj.strName, obj.strTag, obj.nLayer);
+	return strMsg;
+}
 
 //함수의 매개변수로 원본을 전달할때는 참조자를 쓰면 코드가 깔끔하다.
-void InitGameObject(SGameObject& obj, const char* name, const char* tag, int layer)
+void InitGameObjectRef(SGameObject& obj, const char* name, const char* tag, int layer)
 {
 	strcpy_s(obj.strName, name);//->: 구조체의 포인터에서는 멤버를 접근할때 사용
 	strcpy_s(obj.strTag, tag);
 	obj.nLayer = layer;
 }
 
-void ToStringGameObject(char* strMsg, int size, const SGameObject& obj)
+void ToStringGameObjectRef(char* strMsg, int size, const SGameObject& obj)
 {
 	sprintf_s(strMsg, size,"Name:%s, Tag:%s, Layer:%d", obj.strName, obj.strTag, obj.nLayer);
 }
@@ -48,16 +48,16 @@ void ToStringGameObject(char* strMsg, int size, const SGameObject& obj)
 enum E_LAYER { Player = 8, Monster, Bullet };
 //enum E_TAG { Player = 8, Monster, Bullet };
 
-void StructTestMain()
+void StructRefTestMain()
 {
 	SGameObject sGameObject = {};// = { "eagle","Monster",0 };
-	InitGameObject(sGameObject, "eagle", "Monster", E_LAYER::Monster);
+	InitGameObjectRef(sGameObject, "eagle", "Monster", E_LAYER::Monster);
 	printf("Name:%s\n", sGameObject.strName);
 	printf("Tag:%s\n", sGameObject.strTag);
 	printf("Layer:%d\n", sGameObject.nLayer);
 	//printf("%s",ToStringGameObject(sGameObject));
 	char strMsg[2048];
-	ToStringGameObject(strMsg, sizeof(strMsg), sGameObject);
+	ToStringGameObjectRef(strMsg, sizeof(strMsg), sGameObject);
 	printf("%s", strMsg);
 	//구조체타입의 포인터
 	SGameObject* pGameObject = &sGameObject;
@@ -67,9 +67,33 @@ void StructTestMain()
 	//구조체는 복사된다.
 	SGameObject sCopyObject = sGameObject;
 	//매개변수로 전달될때도 복사되므로, 참조자를 이용하는 것이 효율적이다.
-	ToStringGameObject(strMsg, sizeof(strMsg), sCopyObject);
+	ToStringGameObjectRef(strMsg, sizeof(strMsg), sCopyObject);
 	printf("%s", strMsg);
 }
+
+void StructPtrTestMain()
+{
+	SGameObject sGameObject = {};// = { "eagle","Monster",0 };
+	InitGameObjectPtr(&sGameObject, "eagle", "Monster", E_LAYER::Monster);
+	printf("Name:%s\n", sGameObject.strName);
+	printf("Tag:%s\n", sGameObject.strTag);
+	printf("Layer:%d\n", sGameObject.nLayer);
+	//printf("%s",ToStringGameObject(sGameObject));
+	char strMsg[2048];
+	ToStringGameObjectPtr(strMsg, sizeof(strMsg), &sGameObject);
+	printf("%s", strMsg);
+	//구조체타입의 포인터
+	SGameObject* pGameObject = &sGameObject;
+	printf("Name:%s\n", pGameObject->strName);
+	printf("Tag:%s\n", pGameObject->strTag);
+	printf("Layer:%d\n", pGameObject->nLayer);
+	//구조체는 복사된다.
+	SGameObject sCopyObject = sGameObject;
+	//매개변수로 전달될때도 복사되므로, 참조자를 이용하는 것이 효율적이다.
+	ToStringGameObjectPtr(strMsg, sizeof(strMsg), &sCopyObject);
+	printf("%s", strMsg);
+}
+
 //동적할당: 프로그램 실행중에 메모리를 만들어서 사용하는 것.
 //정적할당: 컴파일러 단에서 메모리가 생성되고 관리되는 것 == 자동할당: 이전까지 배운 모든 변수선언
 void DynamicTestMain()
@@ -95,15 +119,16 @@ void DynamicTestMain()
 
 		free(pBuffur);//동적할당된 메모리 제거
 	}
+
 	//구조체의 메모리크기만큼 할당한다.
 	pMalloc = malloc(sizeof(SGameObject));
 	if (pMalloc)
 	{
 		//할당된 메모리를 구조체로 캐스팅하여 각 멤버에 접근한다.
 		SGameObject* pGameObject = (SGameObject*)pMalloc;
-		InitGameObject(*pGameObject, "test", "test", 0);
+		InitGameObjectPtr(pGameObject, "test", "test", 0);
 		char strMsg[2048];
-		ToStringGameObject(strMsg, sizeof(strMsg), *pGameObject);
+		ToStringGameObjectPtr(strMsg, sizeof(strMsg), pGameObject);
 		printf("%s", strMsg);
 		free(pGameObject);
 	}
