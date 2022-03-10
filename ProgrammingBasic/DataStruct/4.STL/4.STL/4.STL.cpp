f03dd5f8f1b06d004799637243505c6a0c9c19cc
downkhg <downkhg@gmail.com>
2022-03-10 21:51:35 +0900

    STL과 자료구조

--- ProgrammingBasic/DataStruct/4.STL/4.STL/4.STL.cpp
+++ ProgrammingBasic/DataStruct/4.STL/4.STL/4.STL.cpp
@@ -88,8 +88,9 @@ void ListMain()
 	//1.추가 2.삽입 //3.찾기 //4.삭제
 	container.push_back(0); //추가
 	container.push_back(-10); //추가
-	
-	it = container.begin()++; it++; it++; it--; it--;//삽입할 이터레이터 설정 //리스트는 다음노드로만 이동이 가능하다. //이전노드로 이동이 가능하다 -> 이중연결리스트다.
+	container.push_front(-30);//
+	//it = container.begin() + 3; //컴파일오류. 리스트는 다음노드만 접근가능하다.
+	it = container.begin()++; it++; it++; //it--; it--;//삽입할 이터레이터 설정 //리스트는 다음노드로만 이동이 가능하다. //이전노드로 이동이 가능하다 -> 이중연결리스트다.
 	list<int>::iterator itInsert = container.insert(it, -20); //해당 이터레이터에 앞에 값을 삽입함.
 	list<int>::iterator itFind = find(container.begin(), container.end(), 1000); //삭제할 이터레이터 설정
 	if (itFind != container.end()) //마지막값은 존제하지않는다.
@@ -114,30 +115,108 @@ void ListMain()
 //데크: 앞뒤로 자료를 추가/삭제가능, 랜덤접근가능.
 void DequeMain()
 {
-
+	deque<int> container(10);//컨테이너생성시 크기를 지정가능하다.
+	container[0] = 10;
+	for (int i = 0; i < container.size(); i++)
+		container[i] = (container.size() - i) * 10;
+	cout << "Print:";
+	for (int i = 0; i < container.size(); i++)
+		cout << "[" << i << "]" << container[i] << ",";
+	cout << endl;//\n
+	container.resize(5); //배열의 크기를 지정한다.
+	cout << "Print:";
+	for (int i = 0; i < container.size(); i++)
+		cout << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+	//1.추가 2.삽입 //3.찾기 //4.삭제
+	container.push_back(0); //추가
+	container.push_back(-10); //추가
+	container.push_front(-30);
+	deque<int>::iterator it;
+	it = container.begin() + 3; //삽입할 이터레이터 설정
+	deque<int>::iterator itInsert = container.insert(it, -20); //해당 이터레이터에 앞에 값을 삽입함.
+	deque<int>::iterator itFind = find(container.begin(), container.end(), 1000); //삭제할 이터레이터 설정
+	if (itFind != container.end()) //마지막값은 존제하지않는다.
+		cout << "itFind:" << *itFind << endl;
+	container.erase(itInsert); //해당 이터레이터를 배열에서 삭제한다.
+	cout << "PrintPtr:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+	reverse(container.begin(), container.end());
+	cout << "Reverse:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+	container.clear(); //모두삭제
+	cout << "Clear:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
 }
 //스택: 뒤에서 추가되고 뒤에서 꺼냄.
 //재귀함수에서 이전 함수를 호출할때마다 스택에 쌓임.
 //문자열뒤집기 -> 문자배열 -> apple -> elppa
 void StackMain()
 {
-
+	char strApple[] = "Apple";
+	stack<char> cStack;
+	cout << "Apple:" << strApple << endl;
+	int nIdx = 0;
+	while (strApple[nIdx]!='\0')
+	{
+		cStack.push(strApple[nIdx]);
+		nIdx++;
+	}
+	nIdx = 0;
+	while (cStack.empty() == false)
+	{
+		strApple[nIdx] = cStack.top();
+		cStack.pop();
+		nIdx++;
+	}
+	cout <<"Reverse:"<< strApple << endl;
 }
 //큐: 뒤에서 추가하고 앞에서 꺼냄.
 //메세지큐: 이벤트가 발생한 순서대로 저장하는 공간.
 //입력된 순서대로 명령어 처리하기
 void QueueMain()
 {
-
+	char cInput = '\0';
+	queue<char> cQueue;
+	
+	while (cInput != 'x')
+	{
+		cin >> cInput;
+		cQueue.push(cInput);
+	}
+	cout << "Message:" << endl;
+	while (cQueue.empty() == false)
+	{
+		cout << cQueue.front() << endl;
+		cQueue.pop();
+	}
 }
 //우선순위큐: 우선순위가 높은 원소가 먼저나감(힙)
 //무작위로 데이터를 넣었을때 어떤 순서대로 데이터가 나오는가? 큰값부터 나온다.
 void PriorytyQueueMain()
 {
+	priority_queue<int> cPriorytyQueue;
 
+	cPriorytyQueue.push(100);
+	cPriorytyQueue.push(20);
+	cPriorytyQueue.push(50);
+	cPriorytyQueue.push(80);
+	cPriorytyQueue.push(10);
+
+	while (cPriorytyQueue.empty() == false)
+	{
+		cout << cPriorytyQueue.top() << endl;
+		cPriorytyQueue.pop();
+	}
 }
 //맵: 사전식으로 데이터를 찾을수있다.
-//해당영어단어를 넣으면 한국어 결과가 나온다.
+//해당영어단어를 넣으면 한국어 결과가 나온다.//균형(레드블랙)이진트리
 void MapMain()
 {
 	map<string, string> mapDic;
@@ -150,7 +229,7 @@ void MapMain()
 	cout << mapDic["try"] << endl;
 	cout << mapDic["note"] << endl;
 }
-//셋: 순서없이 데이터를 넣는다. 데이터는 순서와 상관없이 데이터를 찾는다.
+//셋: 순서없이 데이터를 넣는다. 데이터는 순서와 상관없이 데이터를 찾는다.//균형(레드블랙)이진트리
 void SetMain()
 {
 	set<int> setData;
@@ -167,7 +246,7 @@ void SetMain()
 		cout << *it << ",";
 	cout << endl;
 }
-//해시맵: 해시테이블
+//해시맵: 해시테이블 
 void HashMapMain()
 {
 	unordered_map<string, string> mapDic;
@@ -183,11 +262,11 @@ void HashMapMain()
 void main()
 {
 	//VectorMain();
-	ListMain();
+	//ListMain();
 	//DequeMain();
 	//StackMain();
 	//QueueMain();
 	//PriorytyQueueMain();
 	//MapMain();
-	//SetMain();
+	SetMain();
 }
