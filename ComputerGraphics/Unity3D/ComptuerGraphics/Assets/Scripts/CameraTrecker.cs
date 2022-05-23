be7bd17021e04dfc35dc707e35cc3efaadd2c1c7
downkhg <downkhg@gmail.com>
2022-05-23 21:43:55 +0900

    지형과 컬링, 쿼트트리

--- ComputerGraphics/Unity3D/ComptuerGraphics/Assets/Scripts/CameraTrecker.cs
+++ ComputerGraphics/Unity3D/ComptuerGraphics/Assets/Scripts/CameraTrecker.cs
@@ -13,17 +13,22 @@ public class CameraTrecker : MonoBehaviour
     {
         
     }
-
+    float m_fAngle;
     // Update is called once per frame
     void Update()
     {
         Vector3 vDir = m_objTarget.transform.forward;
         Vector3 vPos = m_objTarget.transform.position;
 
-        vDir = Quaternion.AngleAxis(1, m_vAsix) * vDir;
+        vDir = Quaternion.AngleAxis(m_fAngle, m_vAsix) * vDir;
         Vector3 vResult = vPos + vDir * m_fDist;
 
         this.transform.position = vResult;
         this.transform.LookAt(m_objTarget.transform);
+
+        m_fAngle++;
+
+        if (m_fAngle < 360)
+            m_fAngle -= 360;
     }
 }
